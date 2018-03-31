40d73eca0555b30f8afd241ed21c6b28276691d6
grafikyusva <33680479+grafikyusva@users.noreply.github.com>
2018-03-31 11:45:51 +0700

    update push command OK AND ER

---
+++ ITI.GateIn.Console/ITI.GateIn.Console.UI/PushCommand.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.GateIn.Console.UI
+{
+    public class PushCommand
+    {
+
+        public bool  PushOK(Terminal paramTerminal )
+        {
+            bool result = false;
+            //if using paramterminal remark below syntac
+            var tn = new Terminal("192.168.15.161", 8023, 10, 80, 40); // hostname, port, timeout [s], width, height
+            tn.Connect(); // physcial connection
+            do
+            {
+                tn.SendResponse("<ACTION>OK!</ACTION>", true); // send dir command
+                result = true;
+            }
+            while (false);
+
+            //if using paramterminal remark below syntac
+            tn.Close(); // physically close on TcpClient
+            return result;
+        }
+        public bool PushER(Terminal paramTerminal)
+        {
+            bool result = false;
+            //if using paramterminal remark below syntac
+            var tn = new Terminal("192.168.15.161", 8023, 10, 80, 40); // hostname, port, timeout [s], width, height
+            tn.Connect(); // physcial connection
+            do
+            {
+                tn.SendResponse("<ACTION>ER!</ACTION>", true); // send dir command
+                result = true;
+            }
+            while (false);
+
+            //if using paramterminal remark below syntac
+            tn.Close(); // physically close on TcpClient
+            return result;
+        }
+
+    }
+}
