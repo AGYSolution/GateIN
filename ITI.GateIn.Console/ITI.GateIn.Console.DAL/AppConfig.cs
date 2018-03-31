f876a63ff1c4881d773140aca044a4aed83c3b3f
grafikyusva <33680479+grafikyusva@users.noreply.github.com>
2018-03-31 12:48:32 +0700

    add logic gate in

---
+++ ITI.GateIn.Console/ITI.GateIn.Console.DAL/AppConfig.cs
@@ -0,0 +1,98 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace ITI.GateIn.Console.DAL
+{
+    public class AppConfig
+    {
+        public const string ConnectionString = "Server={0};Port={1};UserId={2};Password={3};Database={4};";
+
+        public static NpgsqlConnection GetLoginConnection()
+        {
+            string loginServer = "localhost";
+            string port = "5432";
+            string userId = "agys";
+            string password = "agys123";
+            string database = "ictslogin";
+            if (ConfigurationManager.AppSettings["LoginServer"] != null)
+            {
+                loginServer = ConfigurationManager.AppSettings["LoginServer"];
+            }
+            if (ConfigurationManager.AppSettings["Port"] != null)
+            {
+                port = ConfigurationManager.AppSettings["Port"];
+            }
+            if (ConfigurationManager.AppSettings["UserId"] != null)
+            {
+                port = ConfigurationManager.AppSettings["UserId"];
+            }
+            if (ConfigurationManager.AppSettings["LoginDatabase"] != null)
+            {
+                port = ConfigurationManager.AppSettings["LoginDatabase"];
+            }
+            string connectionString = string.Format(ConnectionString,
+                loginServer,
+                port,
+                userId,
+                password,
+                database
+            );
+            NpgsqlConnection npgsqlConnection = new NpgsqlConnection(connectionString);
+            return npgsqlConnection;
+        }
+
+        public static NpgsqlConnection GetConnection()
+        {
+            string loginServer = "localhost";
+            string port = "5432";
+            string userId = "agys";
+            string password = "agys123";
+            string database = "icts";
+            if (ConfigurationManager.AppSettings["LoginServer"] != null)
+            {
+                loginServer = ConfigurationManager.AppSettings["LoginServer"];
+            }
+            if (ConfigurationManager.AppSettings["Port"] != null)
+            {
+                port = ConfigurationManager.AppSettings["Port"];
+            }
+            if (ConfigurationManager.AppSettings["UserId"] != null)
+            {
+                userId = ConfigurationManager.AppSettings["UserId"];
+            }
+            if (ConfigurationManager.AppSettings["Database"] != null)
+            {
+                database = ConfigurationManager.AppSettings["Database"];
+            }
+            string connectionString = string.Format(ConnectionString,
+                loginServer,
+                port,
+                userId,
+                password,
+                database
+            );
+            NpgsqlConnection npgsqlConnection = new NpgsqlConnection(connectionString);
+            return npgsqlConnection;
+        }
+
+        public static NpgsqlConnection GetUserConnection()
+        {
+
+            string connectionString = string.Format(ConnectionString + "Pooling = True; MaxPoolSize = 60;",
+
+                "localhost",
+                "5432",
+                                             //appIdentity.UserId,
+                                             "admin",
+                                            "",
+                                            "");
+            NpgsqlConnection npgsqlConnection = new NpgsqlConnection(connectionString);
+            return npgsqlConnection;
+        }
+    }
+}
